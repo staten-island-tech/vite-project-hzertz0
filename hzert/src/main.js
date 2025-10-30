515efea9d6a9592bf703d32d6096f4bb6f15b4a7
hzertz0 <139808204+hzertz0@users.noreply.github.com>
2025-10-30 11:02:16 -0700

    testing stuff

--- hzert/src/main.js
+++ hzert/src/main.js
@@ -1,3 +1,9 @@
-function mode(){
-    mode = document.querySelector(".mode")
-}
+document.querySelector(".btn").addEventListener("click", function () {
+  if (document.body.classList.contains("light")) {
+    document.body.classList.add("dark");
+    document.body.classList.remove("light");
+  } else {
+    document.body.classList.add("light");
+    document.body.classList.remove("dark");
+  }
+});
