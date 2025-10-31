93995162ef865a984391ddfa3f8887e61eb56ef9
hzertz0 <139808204+hzertz0@users.noreply.github.com>
2025-10-31 11:02:15 -0700

    e

--- hzert/src/main.js
+++ hzert/src/main.js
@@ -1,9 +1,28 @@
+const songs = [
+    {
+    name: "Grayton",
+    length: "2:00",
+    image: "src/assets/us.png"
+    },
+]
+
+songs.forEach((song) => {
+  document.querySelector(".container").insertAdjacentHTML(
+    "afterbegin",
+    `<div class="card">
+            <h2>${song.name}</h2>
+            <img src="${song.image}" />
+            <p>Length: ${song.length}</p>
+        </div>`
+  );
+});
+
 document.querySelector(".btn").addEventListener("click", function () {
-  if (document.body.classList.contains("light")) {
-    document.body.classList.add("dark");
-    document.body.classList.remove("light");
-  } else {
+  if (document.body.classList.contains("dark")) {
     document.body.classList.add("light");
     document.body.classList.remove("dark");
+  } else {
+    document.body.classList.add("dark");
+    document.body.classList.remove("light");
   }
 });
